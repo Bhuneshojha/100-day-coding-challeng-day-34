f138fa910e4b770dcf5faf12ce834f19e52ef0cd
Bhunesh ojha108 <149822963+Bhuneshojha@users.noreply.github.com>
2024-04-22 15:55:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+//          100 days of coding challenge ( DAY 34 )
+Object.defineProperty(exports, "__esModule", { value: true });
+// Question 01
+const squareRoot = Math.sqrt(169);
+console.log(squareRoot);
+// This line effectively calculates and shows the square root of 144.
+// Question 02
+function getRandomInt() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(getRandomInt());
+// Question 03 
+// --Calculates the absolute difference between -5 and 5
+const difference = Math.abs(-5 - 5);
+console.log(difference);
+// --This line shows the absolute value of the difference between -5 and 5, ignoring the sign.
---
+++ main.ts
@@ -0,0 +1,16 @@
+//          100 days of coding challenge ( DAY 34 )
+
+// Question 01
+const squareRoot:number = Math.sqrt(169);
+console.log(squareRoot)
+// This line effectively calculates and shows the square root of 144.
+// Question 02
+function getRandomInt(): number{
+  return Math.floor(Math.random() * 10) + 1;
+}
+console.log(getRandomInt())
+// Question 03 
+// --Calculates the absolute difference between -5 and 5
+const difference:number = Math.abs(-5 -5);
+console.log(difference)
+// --This line shows the absolute value of the difference between -5 and 5, ignoring the sign.
